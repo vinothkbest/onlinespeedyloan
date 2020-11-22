686188471e2f01f6b059e0e0acb614ee68f55787
VINOTHKUMAR K <56819946+vinothkbest@users.noreply.github.com>
2020-11-22 22:45:27 +0530

    Update dashboard.php

--- app/Views/admin/dashboard.php
+++ app/Views/admin/dashboard.php
@@ -1,26 +1,125 @@
 <?= $this->extend('admin/part/template') ?>
 
 <?= $this->section('content') ?>
-    <div class="panel-body counts-panel">
-            <div class="row">
-                <div class="col-md-4" style="text-align: center; height: 70px;">
-                    <h3>Total Number of Approved Users</h3>
-                    <p><?= count($auhorizedcodedetails['approvedusers']) ?></p>
-                
-            </div>
-            <div class="col-md-4" style="text-align: center; height: 70px;">
-                
-                    <h3>Total Number of Third Party Websites</h3>
-                    
-                
-            </div>
-            <div class="col-md-4" style="text-align: center; height: 70px;">
-                
-                    <h3>Total Number of Authorization Codes</h3>
-                    <p><?= count($auhorizedcodedetails['totalusage']) ?></p>
-            
-            </div>
-            </div>
+
+<div class="dashboard-content">
+  
+  <div class="tile-panel">
+
+    <div class="approved-users tile">
+      <div class="title">
+        Total Approved Users
+      </div>
+      <div class="counts users">
+        
+      </div>
     </div>
+
+    <div class="approved-codes tile">
+      <div class="title">
+        Total Authorized Codes
+      </div>
+      <div class="counts codes">
+      </div>
+    </div>
+
+    <div class="third-party-websites tile">
+      <div class="title">
+        Total Third Party Websites
+      </div>
+      <div class="counts websites">
+        
+        
+      </div>
+    </div>
+
+  </div>
+
+</div>
+<p hidden class="approved-users-count"></p>
+<p hidden class="approved-codes-count"></p>
+<p hidden class="websites-count"></p>
+<style type="text/css">
+  .tile-panel{
+    width:100%;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+  }
+  .tile{
+    width:250px;
+    height: 250px;
+    background: #315ea2;
+    border-radius: 15px;
+    color: white;
+    opacity: 0.8;
+  }
+  .title, .counts{
+    text-align: center;
+    margin-top: 50px;
+    font-weight: bolder;
+    font-size: 20px
+  }
+  .counts{
+    
+     font-size: 40px
+
+  }
+  
+</style>
+
+<script src="<?= base_url('admin/js/jquery.js')?>"></script>
+<script type="text/javascript">
+    
+    var approvedusers = <?= count($auhorizedcodedetails['approvedusers']) ?>;
+    var approvedcodes = <?= count($auhorizedcodedetails['totalusage']) ?>;
+    var websites = 90;
+
+    let i = 0, j = 0, k = 0;
+
+    setInterval(users, 100);
+
+    function users(){
+
+       if( approvedusers > i ) {
+
+         i = i+1;
+
+         $('.users').text(i);
+
+       }
+
+    }
+
+    setInterval(codes, 100);
+
+    function codes(){
+
+       if( approvedcodes > j ) {
+
+         j = j+1;
+
+         $('.codes').text(j);
+
+       }
+
+    }
+  
+    setInterval(websitesusage, 100);
+
+    function websitesusage(){
+
+       if( websites > k ) {
+
+         k = k+1;
+
+         $('.websites').text(k);
+
+       }
+
+    }
+
+</script>
 
 <?= $this->endSection() ?>
